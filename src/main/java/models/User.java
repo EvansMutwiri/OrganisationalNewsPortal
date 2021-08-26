522bcd929ef3205a6c224620f8b5f38d4bf1f590
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-26 11:55:49 +0300

    User class methods

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,16 +1,71 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
     private String username;
     private String position;
-    private String department;
+    private int departmentId;
     private String role; //admin or user
     private int id;
 
-    public User(String username, String position, String department, String role) {
+    public User(String username, String position, int departmentId, String role) {
         this.username = username;
         this.position = position;
-        this.department = department;
+        this.departmentId = departmentId;
         this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return departmentId == user.departmentId && id == user.id && username.equals(user.username) && position.equals(user.position) && role.equals(user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, position, departmentId, role, id);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,53 @@ public class UserTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void getPosition() {
+    }
+
+    @Test
+    public void setPosition() {
+    }
+
+    @Test
+    public void getDepartmentId() {
+    }
+
+    @Test
+    public void setDepartmentId() {
+    }
+
+    @Test
+    public void getRole() {
+    }
+
+    @Test
+    public void setRole() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
     }
 }
