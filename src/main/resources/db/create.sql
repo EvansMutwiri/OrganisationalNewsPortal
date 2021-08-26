ebc5a2b4c4f679ea3f2f466e56852f030a199f15
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-26 12:09:57 +0300

    SQL file with user table

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -2,13 +2,24 @@ SET MODE PostgreSQL;
 
 CREATE TABLE IF NOT EXISTS departments (
     id int serial PRIMARY KEY auto_increment,
+    name VARCHAR,
     description VARCHAR,
     noEmployees int
+
 )
 
 CREATE TABLE IF NOT EXISTS news (
     id int serial PRIMARY KEY auto_increment,
     heading VARCHAR,
     content VARCHAR
+
+)
+
+CREATE TABLE IF NOT EXISTS users (
+    id int serial PRIMARY KEY auto_increment,
+    username VARCHAR,
+    position VARCHAR,
+    department VARCHAR,
+    role VARCHAR
 
 )
