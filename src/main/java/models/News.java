58e87857f108362a8e5bcc650b088243598c9ffe
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-26 11:26:36 +0300

    News constructors and tests

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -3,11 +3,26 @@ package models;
 public class News {
     private String heading;
     private String content;
+    private String writtenBy;
+    private String type;
+    private int departmentId;
+
     private int id;
 
-    public News(String heading, String content) {
+
+    public News(String heading, String content, String writtenBy, String type) {
         this.heading = heading;
         this.content = content;
+        this.writtenBy = writtenBy;
+        this.type = "Organisational news";
+    }
+
+    public News(String heading, String content, String writtenBy, String type, int departmentId) {
+        this.heading = heading;
+        this.content = content;
+        this.writtenBy = writtenBy;
+        this.type = "Departmental news";
+        this.departmentId = departmentId;
     }
 
     public String getHeading() {
@@ -24,5 +39,25 @@ public class News {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public void setWittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public String getType() {
+        return  type;
+    }
+
+    public void setType(String Organisational_news) {
+        this.type = Organisational_news;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
     }
 }
--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewsTest {
-    News newsAlpha = new News("Finance","make payments");
+    News newsOrg =  new News("A G M", "will be virtual", "Evans", "Organisational news");
+    News newsAlpha = new News("Finance","make payments", "", "Departmental news", 1);
 
     @Before
     public void setUp() throws Exception {
@@ -40,5 +41,46 @@ class NewsTest {
     public void setContentCorrectly() throws Exception {
         newsAlpha.setContent("advertise");
         assertNotEquals("make payments", newsAlpha.getContent());
+    }
+
+    @Test
+    public void getWrittenBy() {
+
+        News newsOrg =  new News("A G M", "will be virtual", "Evans", "Organisational news");
+        assertEquals("Evans", newsOrg.getWrittenBy());
+    }
+
+    @Test
+    public void setWrittenBy() {
+        newsOrg.setWittenBy("Smith");
+        assertEquals("Smith", newsOrg.getWrittenBy());
+    }
+
+    @Test
+    public void getType() {
+//        News newsOrg =  new News("A G M", "will be virtual", "Evans", "Organisational news");
+        assertEquals("Organisational news", newsOrg.getType());
+        assertEquals("Departmental news", newsAlpha.getType());
+    }
+
+    @Test
+    public void setType() {
+        News newsOrg =  new News("A G M", "will be virtual", "Evans", "");
+        newsOrg.setType("Organisational news");
+        assertEquals("Organisational news", newsOrg.getType());
+
+    }
+
+    @Test
+    public void getDepartmentId() {
+        newsAlpha.getDepartmentId();
+        assertEquals(1, newsAlpha.getDepartmentId());
+    }
+
+    @Test
+    public void setDepartmentId() {
+        News newsAlpha = new News("Finance","make payments", "", "Departmental news", 1);
+        News newsTest = new News("Finance","make payments", "", "Departmental news", 2);
+        assertEquals(2, newsTest.getDepartmentId());
     }
 }
